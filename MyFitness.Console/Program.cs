8960d815f887ec11a1e97ff4f7578c69b65311be
Swift <rafchik37@gmail.com>
2023-05-04 16:38:45 +0200

    Добавьте файлы проекта.

---
+++ MyFitness.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFitness.Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
