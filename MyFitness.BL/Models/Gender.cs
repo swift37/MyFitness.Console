0837439ea8fc03dea598140dd14d5e67aeae2ee0
Swift <rafchik37@gmail.com>
2023-05-04 16:40:23 +0200

    Модель пользователя и класс Gender

---
+++ MyFitness.BL/Models/Gender.cs
@@ -0,0 +1,31 @@
+﻿namespace MyFitness.BL.Models
+{
+    /// <summary>
+    /// Gender.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public string? Name { get; }
+
+        /// <summary>
+        /// Create new gender.
+        /// </summary>
+        /// <param name="name">Gender name.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name)) 
+                throw new ArgumentNullException(nameof(name));
+            
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
---
+++ MyFitness.BL/Models/User.cs
@@ -0,0 +1,72 @@
+﻿namespace MyFitness.BL.Models
+{
+    /// <summary>
+    /// User.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public string? Name { get; }
+
+        /// <summary>
+        /// Gender.
+        /// </summary>
+        public Gender? Gender { get; }
+
+        /// <summary>
+        /// Birth date.
+        /// </summary>
+        public DateTime BirthDate { get; }
+
+        /// <summary>
+        /// Weight
+        /// </summary>
+        public double Weight { get; }
+
+        /// <summary>
+        /// Height.
+        /// </summary>
+        public double Height { get; }
+
+        /// <summary>
+        /// Create new user.
+        /// </summary>
+        /// <param name="name">User name.</param>
+        /// <param name="gender">User gender.</param>
+        /// <param name="birthDate">User birthday.</param>
+        /// <param name="weight">User weight.</param>
+        /// <param name="height">User height.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="InvalidDataException"></exception>
+        public User(string name, Gender gender, DateTime birthDate, double weight, double height)
+        {
+            #region Parameters validation
+
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException(nameof(name));
+            if (gender == null)
+                throw new ArgumentNullException(nameof(gender));
+            if (birthDate < DateTime.Parse("01.01.1900") || birthDate > DateTime.Now)
+                throw new InvalidDataException(nameof(birthDate));
+            if (weight < 0.0)
+                throw new InvalidDataException(nameof(weight));
+            if (height < 0.0)
+                throw new InvalidDataException(nameof(height));
+            
+            #endregion
+
+            Name = name;
+            Gender = gender;
+            BirthDate = birthDate;
+            Weight = weight;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
