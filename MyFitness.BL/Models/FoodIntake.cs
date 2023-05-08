711ffa06b21ba60334267dba3ad0d3ee82a9a39d
Swift <rafchik37@gmail.com>
2023-05-08 19:29:30 +0200

    Модели блюда(продукта) и приема пищи.

---
+++ MyFitness.BL/Models/FoodIntake.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFitness.BL.Models
+{
+    /// <summary>
+    /// Food intake.
+    /// </summary>
+    [DataContract]
+    public class FoodIntake
+    {
+        /// <summary>
+        /// Food intake moment.
+        /// </summary>
+        [DataMember(Name = "moment")]
+        public DateTime Moment { get; set; }
+
+        /// <summary>
+        /// Foods and foods weight list.
+        /// </summary>
+        [DataMember(Name = "meals")]
+        public Dictionary<Meal, double>? Meals { get; set; }
+
+        /// <summary>
+        /// User.
+        /// </summary>
+        [DataMember(Name = "user")]
+        public User? User { get; set; }
+
+        /// <summary>
+        /// Create new food intake.
+        /// </summary>
+        /// <param name="user">Username.</param>
+        /// <param name="moment">Food intake moment.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public FoodIntake(User? user, DateTime moment)
+        {
+            User = user ?? throw new ArgumentNullException(nameof(user)); 
+            Moment = moment;
+            Meals = new Dictionary<Meal, double>();
+        }
+
+        /// <summary>
+        /// Add new meal to food intake.
+        /// </summary>
+        /// <param name="food">Meal.</param>
+        /// <param name="weight">Meal weight.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public void Add(Meal food, double weight)
+        {
+            if( Meals is null || food.Name is null) throw new ArgumentNullException(nameof(food));
+
+            var product = Meals.Keys.FirstOrDefault(f => f.Name.Equals(food.Name));
+
+            if (product is null)
+                Meals.Add(food, weight);
+            else
+                Meals[product] += weight;
+        }
+    }
+}
---
+++ MyFitness.BL/Models/Meal.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFitness.BL.Models
+{
+    /// <summary>
+    /// Meal.
+    /// </summary>
+    [DataContract]
+    public class Meal
+    {
+        private double ProteinsOneGr => Proteins / 100;
+
+        private double FatsOneGr => Fats / 100;
+
+        private double CarbohydratesOneGr => Carbohydrates / 100;
+
+        private double CaloriesOneGr => Calories / 100;
+
+        /// <summary>
+        /// Name.
+        /// </summary>
+        [DataMember(Name = "name")]
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Proteins per 100 grams of product.
+        /// </summary>
+        [DataMember(Name = "proteins")]
+        public double Proteins { get; set; }
+
+        /// <summary>
+        /// Fats per 100 grams of product.
+        /// </summary>
+        [DataMember(Name = "fats")]
+        public double Fats { get; set; }
+
+        /// <summary>
+        /// Carbohydrates per 100 grams of product.
+        /// </summary>
+        [DataMember(Name = "carbohydrates")]
+        public double Carbohydrates { get; set; }
+
+        /// <summary>
+        /// Calories per 100 grams of product.
+        /// </summary>
+        [DataMember(Name = "calories")]
+        public double Calories { get; set; }
+
+        /// <summary>
+        /// Create new meal.
+        /// </summary>
+        /// <param name="name">Meal name.</param>
+        public Meal(string? name) : this(name, 0, 0, 0, 0) { }
+
+        /// <summary>
+        /// Create new meal.
+        /// </summary>
+        /// <param name="name">Meal name.</param>
+        /// <param name="calories">Calories.</param>
+        /// <param name="proteins">Proteins.</param>
+        /// <param name="fats">Fats.</param>
+        /// <param name="carbohydrates">Carbohydrates.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Meal(string? name, double calories, double proteins, double fats, double carbohydrates)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Calories = calories;
+            Proteins = proteins;
+            Fats = fats;
+            Carbohydrates = carbohydrates;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Calories} kcal. per 100 grams";
+        }
+    }
+}
